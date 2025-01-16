af3a46aac90b2ef65db0e1d2f674ad9619764e61
gilsonlevi <gilsonlevi328@gmail.com>
2025-01-16 12:02:18 -0300

    Melhorando ponteiros

--- ponteiros/ponteirosfunc.c
+++ ponteiros/ponteirosfunc.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
-int funcao(int c, int d){
-    return c + d;
-}
-
 int main()
 {
-    int A = 10;
-    int B = 50;
+    int A,B; //Criando as variaveis A e B
     
-    int* ptrA = &B;
-    int* ptrB = &A;
+    A = 10; //Adicionando o valor 10 a variavel A
+    B = 20; //Adicionando o valor 20 a variavel B
     
-    int r = funcao(*ptrA, *ptrB);
     
-    printf("%i", r);
+    int* ptrA = &A; //Criando um apontador que aponta para o endereço da variavel A
+    
+    int* ptrB = &B; //Criando um apontador que aponta para o endereço da variavel B
+    
+    printf("Valor de A %i \n", A);
+    printf("Valor de A %i \n", B);
+    
+    // printf("Endereço de A a partir do apontador: %p \n", ptrA);
+    // printf("Endereço de B a partir do apontador: %p \n", ptrB);
+    
+    // printf("Valor de A a partir do apontador: %i \n", *ptrA);
+    // printf("Valor de B a partir do apontador: %i \n", *ptrB);
+    
+    printf("Endereço de A a partir do apontador: %p \n", ptrA);
+    printf("Endereço do apontador a partir do apontador: %p \n", &ptrA);
+    
     
     return 0;
 }
